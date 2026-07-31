f6fdbf4e7c8ac7173cbcc8dac9be293cbebdb1b6
Atom <arbinneupane2000@gmail.com>
2026-07-31 15:12:19 +0000

    Complete Task 3 encryption and update coursework implementations

--- task3_file_security/main.c
+++ task3_file_security/main.c
@@ -1,65 +1,98 @@
 #include <stdio.h>
 #include "auth.h"
 #include "file_ops.h"
+#include "logger.h"
+#include "encryption.h"
+
 
 int main()
 {
-    int choice;
 
-    printf("=====================================\n");
+    printf("=================================\n");
     printf(" Secure File Management System\n");
-    printf("=====================================\n\n");
+    printf("=================================\n");
 
-    if (!login())
+
+    if(login() == 0)
     {
-        printf("Access denied.\n");
         return 1;
     }
 
-    while (1)
+
+    int choice;
+
+
+    while(1)
     {
+
         printf("\n========== MENU ==========\n");
         printf("1. Create File\n");
         printf("2. Read File\n");
         printf("3. Write File\n");
         printf("4. Delete File\n");
-        printf("5. Change File Permission\n");
-        printf("6. Exit\n");
-        printf("==========================\n");
+        printf("5. Change Permission\n");
+        printf("6. Encrypt File\n");
+        printf("7. Decrypt File\n");
+        printf("8. Exit\n");
+
 
         printf("Enter choice: ");
-        scanf("%d", &choice);
+        scanf("%d",&choice);
 
-        switch (choice)
+
+
+        switch(choice)
         {
+
             case 1:
                 createFile();
                 break;
+
 
             case 2:
                 readFile();
                 break;
 
+
             case 3:
                 writeFile();
                 break;
+
 
             case 4:
                 deleteFile();
                 break;
 
+
             case 5:
                 changePermission();
                 break;
 
+
             case 6:
+                encryptFile();
+                break;
+
+
+            case 7:
+                decryptFile();
+                break;
+
+
+            case 8:
+
                 printf("Exiting system...\n");
                 return 0;
 
+
             default:
-                printf("Invalid choice.\n");
+
+                printf("Invalid option.\n");
+
         }
+
     }
+
 
     return 0;
 }
